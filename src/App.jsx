9200814dea878511a731049e02bd414a06e738f7
Blueboltz <vincentmariscotescleofe@gmail.com>
2023-02-19 15:38:51 +0800

    added boilerplate with Vite + React

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <div>
-        <h1 className="text-center text-gray-600 text-2xl">Hello Vite + React</h1>
+        <h1 className="text-center text-gray-600 text-2xl font-bold">Hello Vite + React</h1>
     </div>
   )
 }
